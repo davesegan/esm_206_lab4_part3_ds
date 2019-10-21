50e1d8826ebfb660e9d35d89c9496091c3d9bdf4
davesegan <davesegan@gmail.com>
2019-10-21 13:57:46 -0700

    disease

--- part_3_ds.R
+++ part_3_ds.R
@@ -10,5 +10,14 @@ db <- read_csv(here("data", "disease_burden.csv")) %>%
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
 
-view(db)
+
+# subset
+db_subset <- db %>% 
+  filter(country_name %in% c("United States", "Japan",
+                             "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+# create basic line graph
+ggplot(db_subset, aes(x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
 
